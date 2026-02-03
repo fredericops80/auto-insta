7892fd0e241a59556e57fa94b09261bed0bfe9db
fredericops80 <fredericopimentel@hotmail.com.br>
2026-02-03 23:14:59 +0000

    chore: map backend env vars to client in next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    // Exposing server variables to the client bundle
+    NEXT_PUBLIC_SUPABASE_URL: process.env.SUPABASE_PROJECT_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.SUPABASE_API_KEY,
+  },
 };
 
 export default nextConfig;
